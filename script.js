9f9b622fc9ba78127c74ab3c128b3776c8a88593
Joshua Majait <entertainment.ripple@gmail.com>
2017-12-11 04:01:37 +0800

    Adding the final sorting algos

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function Graph(value0, value1, value2, value3){
     this.positionY = value2;
     this.X = 100;
     this.Y = 100;
+    this.height = 300;
     this.arr = []
     switch(value3){
         case 0://sorted
@@ -56,13 +57,33 @@ function Graph(value0, value1, value2, value3){
                 break;
             case 4: 
                 for(var l = 0; l<this.X; l++){
-                    gaps = [1,4,10,23,57];
+                    gaps = [57,23,10,4,1];
                     temp.push(shellSort(this.arr.slice(0,l)));
                 }
                 break;
             case 5: 
                 for(var l = 0; l<this.X; l++){
                     temp.push(bucketSort(this.arr.slice(0,l)));
+                }
+                break;
+            case 6: 
+                for(var l = 0; l<this.X; l++){
+                    temp.push(radixCountingSort(this.arr.slice(0,l)));
+                }
+                break;
+            case 7: 
+                for(var l = 0; l<this.X; l++){
+                    temp.push(radixInsertionSort(this.arr.slice(0,l)));
+                }
+                break;
+            case 8: 
+                for(var l = 0; l<this.X; l++){
+                    temp.push(mergeSort(this.arr.slice(0,l)));
+                }
+                break;
+            case 9: 
+                for(var l = 0; l<this.X; l++){
+                    temp.push(quickSort(this.arr.slice(0,l)));
                 }
                 break;
         }
@@ -77,7 +98,7 @@ function Graph(value0, value1, value2, value3){
 		context.font = "20px Calibri";
         context.fillText( "0", this.positionX, this.positionY+(this.Y*3)+20);
         context.fillText( "100", this.positionX+(this.X*3), this.positionY+(this.Y*3)+20);
-        context.fillText( "100", this.positionX, this.positionY+20);
+        context.fillText( "300", this.positionX, this.positionY+20);
         context.fillStyle = "#fff";
 		context.font = "30px Calibri";
         context.fillText( this.name, this.positionX, this.positionY+(this.Y*3)+50);
@@ -88,8 +109,8 @@ function Graph(value0, value1, value2, value3){
             context.moveTo(this.positionX, this.positionY+(this.Y*3));
             context.stroke();
             for(var x = 0; x < this.Y; x++){
-                if(this.Y-this.Lines[ctr].arr[x] >= 0)
-                context.lineTo(this.positionX+(x*3), this.positionY+((this.Y-this.Lines[ctr].arr[x])*3));
+                if(this.Y*(this.height/this.Y)-this.Lines[ctr].arr[x] >= 0)
+                context.lineTo(this.positionX+(x*3), this.positionY+((this.Y*(this.height/this.Y)-this.Lines[ctr].arr[x])));
                 context.stroke();
             }
             context.fillStyle = this.Lines[ctr].color;
@@ -108,61 +129,116 @@ function Random(value){
     return Math.floor(Math.random() * value)
 }
 
-var OnSquaredComparisonBC = new Graph("PreSorted Comparison",10,10,0);
+var OnSquaredComparisonBC = new Graph("PreSorted Comparison",10,100,0);
 OnSquaredComparisonBC.addLine("Selection", "#f00", 0);
 OnSquaredComparisonBC.addLine("Bubble", "#0f0", 1);
 OnSquaredComparisonBC.addLine("Insertion", "#00f", 2);
 
-var OnSquaredComparisonWC = new Graph("RevSorted Comparison",400,10,1);
+var OnSquaredComparisonWC = new Graph("RevSorted Comparison",400,100,1);
 OnSquaredComparisonWC.addLine("Selection", "#f00", 0);
 OnSquaredComparisonWC.addLine("Bubble", "#0f0", 1);
 OnSquaredComparisonWC.addLine("Insertion", "#00f", 2);
 
 
-var OnSquaredComparisonAC = new Graph("RandSorted Comparison",800,10,2);
+var OnSquaredComparisonAC = new Graph("RandSorted Comparison",800,100,2);
 OnSquaredComparisonAC.addLine("Selection", "#f00", 0);
 OnSquaredComparisonAC.addLine("Bubble", "#0f0", 1);
 OnSquaredComparisonAC.addLine("Insertion", "#00f", 2);
 
-var ShellSortOptimizationBC = new Graph("PreSorted Comparison",10,500,0);
+var ShellSortOptimizationBC = new Graph("PreSorted Comparison",10,600,0);
 ShellSortOptimizationBC.addLine("Insertion", "#f00",2);
 ShellSortOptimizationBC.addLine("Shell", "#0f0",3);
 
-var ShellSortOptimizationWC = new Graph("RevSorted Comparison",400,500,1);
+var ShellSortOptimizationWC = new Graph("RevSorted Comparison",400,600,1);
 ShellSortOptimizationWC.addLine("Insertion", "#f00",2);
 ShellSortOptimizationWC.addLine("Shell", "#0f0",3);
 
-var ShellSortOptimizationAC = new Graph("RandSorted Comparison",800,500,2);
+var ShellSortOptimizationAC = new Graph("RandSorted Comparison",800,600,2);
 ShellSortOptimizationAC.addLine("Insertion", "#f00",2);
 ShellSortOptimizationAC.addLine("Shell", "#0f0",3);
 
-var ShellGapComparisonShells = new Graph("ShellCiura Comparison", 10, 1000, 2);
+var ShellGapComparisonShells = new Graph("Shell vs Ciura Comparison", 10, 1100, 2);
 ShellGapComparisonShells.addLine("Shell", "#0f0",3);
 ShellGapComparisonShells.addLine("Ciura", "#00f",4);
 
-var BucketSortOptimizationBC = new Graph("PreSorted Comparison", 10 ,1500, 0);
+var BucketSortOptimizationBC = new Graph("PreSorted Comparison", 10 ,1600, 0);
 BucketSortOptimizationBC.addLine("Bucket", "#0f0",5);
 
-var BucketSortOptimizationWC = new Graph("RevSorted Comparison", 400 ,1500, 1);
+var BucketSortOptimizationWC = new Graph("RevSorted Comparison", 400 ,1600, 1);
 BucketSortOptimizationWC.addLine("Bucket", "#0f0",5);
 
-var BucketSortOptimizationAC = new Graph("RandSorted Comparison", 800 ,1500, 2);
-BucketSortOptimizationAC.addLine("Bucket", "#0f0",4);
+var BucketSortOptimizationAC = new Graph("RandSorted Comparison", 800 ,1600, 2);
+BucketSortOptimizationAC.addLine("Bucket", "#0f0",5);
+
+var RadixSortOptimizationBC = new Graph("PreSorted Comparison", 10 ,2100, 0);
+RadixSortOptimizationBC.addLine("RadixCounting", "#0f0",6);
+RadixSortOptimizationBC.addLine("RadixInsertion", "#00f",7);
+
+var RadixSortOptimizationWC = new Graph("RevSorted Comparison", 400 ,2100, 1);
+RadixSortOptimizationWC.addLine("RadixCounting", "#0f0",6);
+RadixSortOptimizationWC.addLine("RadixInsertion", "#00f",7);
+
+var RadixSortOptimizationAC = new Graph("RandSorted Comparison", 800 ,2100, 2);
+RadixSortOptimizationAC.addLine("RadixCounting", "#0f0",6);
+RadixSortOptimizationAC.addLine("RadixInsertion", "#00f",7);
+
+var MergevsQuickBC = new Graph("PreSorted Comparison", 10 ,2600, 0);
+MergevsQuickBC.addLine("Merge", "#0f0",8);
+MergevsQuickBC.addLine("Quick", "#00f",9);
+
+var MergevsQuickWC = new Graph("RevSorted Comparison", 400 ,2600, 1);
+MergevsQuickWC.addLine("Merge", "#0f0",8);
+MergevsQuickWC.addLine("Quick", "#00f",9);
+
+var MergevsQuickAC = new Graph("RandSorted Comparison", 800 ,2600, 2);
+MergevsQuickAC.addLine("Merge", "#0f0",8);
+MergevsQuickAC.addLine("Quick", "#00f",9);
+
 
 function Time(){
     context.fillStyle = "#000";
-    context.fillRect(0,0,1500,2000);
+    context.fillRect(0,0,1500,3000);
 
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+
+    context.fillText("O(n^2) Algorithms (Selection, Bubble, Insertion)", 10, 70);
     OnSquaredComparisonBC.draw();
     OnSquaredComparisonWC.draw();
     OnSquaredComparisonAC.draw();
+
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+    context.fillText("Shell Sort Optimization", 10, 570);
     ShellSortOptimizationBC.draw();
     ShellSortOptimizationWC.draw();
     ShellSortOptimizationAC.draw();
+
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+    context.fillText("Shell vs Ciura", 10, 1070);
     ShellGapComparisonShells.draw();
+
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+    context.fillText("Bucket Sort Optimization", 10, 1570);
     BucketSortOptimizationBC.draw();
     BucketSortOptimizationWC.draw();
     BucketSortOptimizationAC.draw();
+    
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+    context.fillText("Radix Sort Optimization", 10, 2070);
+    RadixSortOptimizationBC.draw();
+    RadixSortOptimizationWC.draw();
+    RadixSortOptimizationAC.draw();
+    
+    context.fillStyle = "#fff";
+    context.font = "50px Calibri";
+    context.fillText("Merge Sort vs Quick Sort", 10, 2570);
+    MergevsQuickBC.draw();
+    MergevsQuickWC.draw();
+    MergevsQuickAC.draw();
 }
 
 setInterval(Time, 1000);
--- sorting.js
+++ sorting.js
@@ -23,7 +23,7 @@ function selectionSort(array) {
         swap(array, i, min);
       }
     }
-    return countOuter+countInner+countSwap;
+    return countOuter+countInner;
   }
 
 function bubbleSort(array) {
@@ -44,7 +44,7 @@ function bubbleSort(array) {
         }
       }
     } while(swapped);
-    return countOuter+countInner+countSwap;
+    return countOuter+countInner;
 }
 
 function insertionSort(array) {
@@ -64,7 +64,7 @@ function insertionSort(array) {
       }
       array[j + 1] = temp;
     }
-    return countOuter+countInner+countSwap;
+    return countOuter+countInner;
   }
 
 var gaps = [];
@@ -103,7 +103,6 @@ function bucketSort(array, bucketSize) {
     
     // Setting min and max values
     array.forEach(function (currentVal) {
-        time++;
         if (currentVal < minValue) {
             minValue = currentVal;
         } else if (currentVal > maxValue) {
@@ -137,63 +136,185 @@ function bucketSort(array, bucketSize) {
 }
 
 
-function radixBucketSort (arr) {
-    var idx1, idx2, idx3, len1, len2, radix, radixKey;
-    var radices = {}, buckets = {}, num, curr;
-    var currLen, radixStr, currBucket;
+var getDigit = function(num,nth){
+    // get last nth digit of a number
+    var ret = 0;
+    while(nth--){
+      ret = num % 10
+      num = Math.floor((num - ret) / 10)
+    }
+    return ret
+ }
+  
+  // radixSort
+function radixCountingSort(list){
+  var time = 0;
+  var max = Math.floor(Math.log10(Math.max.apply(Math,list))),  
+      // get the length of digits of the max value in this array
+      digitBuckets = [],
+      idx = 0;
 
-    len1 = arr.length;
-    len2 = 10;  // radix sort uses ten buckets
+  for(var i = 0;i<max+1;i++){
 
-    // find the relevant radices to process for efficiency        
-    for (idx1 = 0;idx1 < len1;idx1++) {
-      radices[arr[idx1].toString().length] = 0;
+    // rebuild the digit buckets according to this digit
+    digitBuckets = []
+    for(var j = 0;j<list.length;j++){
+      var digit = getDigit(list[j],i+1);
+      
+      digitBuckets[digit] = digitBuckets[digit] || [];
+      digitBuckets[digit].push(list[j]);
     }
 
-    // loop for each radix. For each radix we put all the items
-    // in buckets, and then pull them out of the buckets.
-    for (radix in radices) {          
-      // put each array item in a bucket based on its radix value
-      len1 = arr.length;
-      for (idx1 = 0;idx1 < len1;idx1++) {
-        curr = arr[idx1];
-        // item length is used to find its current radix value
-        currLen = curr.toString().length;
-        // only put the item in a radix bucket if the item
-        // key is as long as the radix
-        if (currLen >= radix) {
-          // radix starts from beginning of key, so need to
-          // adjust to get redix values from start of stringified key
-          radixKey = curr.toString()[currLen - radix];
-          // create the bucket if it does not already exist
-          if (!buckets.hasOwnProperty(radixKey)) {
-            buckets[radixKey] = [];
-          }
-          // put the array value in the bucket
-          buckets[radixKey].push(curr);          
-        } else {
-          if (!buckets.hasOwnProperty('0')) {
-            buckets['0'] = [];
-          }
-          buckets['0'].push(curr);          
+    // rebuild the list according to this digit
+    idx = 0
+    for(var t = 0; t< digitBuckets.length;t++){
+      if(digitBuckets[t] && digitBuckets[t].length > 0){
+        for(j = 0;j<digitBuckets[t].length;j++){
+          list[idx++] = digitBuckets[t][j];
+          time++;
         }
       }
-      // for current radix, items are in buckets, now put them
-      // back in the array based on their buckets
-      // this index moves us through the array as we insert items
-      idx1 = 0;
-      // go through all the buckets
-      for (idx2 = 0;idx2 < len2;idx2++) {
-        // only process buckets with items
-        if (buckets[idx2] != null) {
-          currBucket = buckets[idx2];
-          // insert all bucket items into array
-          len1 = currBucket.length;
-          for (idx3 = 0;idx3 < len1;idx3++) {
-            arr[idx1++] = currBucket[idx3];
-          }
-        }
-      }
-      buckets = {};
     }
   }
+  return time;
+}
+
+function radixInsertionSort (arr) {
+  var time = 0;
+  var idx1, idx2, idx3, len1, len2, radix, radixKey;
+  var radices = {}, buckets = {}, num, curr;
+  var currLen, radixStr, currBucket;
+
+  len1 = arr.length;
+  len2 = 10;  // radix sort uses ten buckets
+
+  // find the relevant radices to process for efficiency        
+  for (idx1 = 0;idx1 < len1;idx1++) {
+    radices[arr[idx1].toString().length] = 0;
+  }
+
+  // loop for each radix. For each radix we put all the items
+  // in buckets, and then pull them out of the buckets.
+  for (radix in radices) {          
+    // put each array item in a bucket based on its radix value
+    len1 = arr.length;
+    for (idx1 = 0;idx1 < len1;idx1++) {
+      curr = arr[idx1];
+      // item length is used to find its current radix value
+      currLen = curr.toString().length;
+      // only put the item in a radix bucket if the item
+      // key is as long as the radix
+      if (currLen >= radix) {
+        // radix starts from beginning of key, so need to
+        // adjust to get redix values from start of stringified key
+        radixKey = curr.toString()[currLen - radix];
+        // create the bucket if it does not already exist
+        if (!buckets.hasOwnProperty(radixKey)) {
+          buckets[radixKey] = [];
+        }
+        // put the array value in the bucket
+        buckets[radixKey].push(curr);          
+      } else {
+        if (!buckets.hasOwnProperty('0')) {
+          buckets['0'] = [];
+        }
+        buckets['0'].push(curr);          
+      }
+    }
+    // for current radix, items are in buckets, now put them
+    // back in the array based on their buckets
+    // this index moves us through the array as we insert items
+    idx1 = 0;
+    // go through all the buckets
+    for (idx2 = 0;idx2 < len2;idx2++) {
+      // only process buckets with items
+      if (buckets[idx2] != null) {
+        currBucket = buckets[idx2];
+        // insert all bucket items into array
+        len1 = currBucket.length;
+        for (idx3 = 0;idx3 < len1;idx3++) {
+          arr[idx1++] = currBucket[idx3];
+          time++;
+        }
+      }
+      time++;
+    }
+    buckets = {};
+  }
+  return time;
+}
+
+var MStime;
+function mergeSort(list){
+  MStime = 0;
+  if(list.length>2)
+    mergeSortMain(list);
+  // else if(list.length>1)
+  //   merge(list[0],list[1]);
+  return MStime;
+}
+
+function mergeSortMain(arr){
+    if (arr.length < 2)
+        return arr;
+ 
+    var middle = parseInt(arr.length / 2);
+    var left   = arr.slice(0, middle);
+    var right  = arr.slice(middle, arr.length);
+    
+    return merge(mergeSortMain(left), mergeSortMain(right));
+}
+
+function merge(left, right){
+  var result = [];
+
+  while (left.length && right.length) {
+      if (left[0] <= right[0]) {
+          result.push(left.shift());
+      } else {
+          result.push(right.shift());
+      }
+      MStime++;
+  }
+
+  while (left.length){
+      result.push(left.shift());
+      MStime++;
+  }
+
+  while (right.length){
+      result.push(right.shift());
+      MStime++;
+  }
+
+  return result;
+}
+
+var QStime = 0;
+
+function quickSort(list){
+  QStime = 0;
+  quicksort(list);
+  return QStime;
+}
+
+function quicksort(arr){
+    if (arr.length == 0)
+        return [];
+ 
+    var left = new Array();
+    var right = new Array();
+    var pivot = arr[0];
+ 
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+           left.push(arr[i]);
+        } else {
+           right.push(arr[i]);
+        }
+        QStime++;
+    }
+    
+    return quicksort(left).concat(pivot, quicksort(right));
+}
+ 
